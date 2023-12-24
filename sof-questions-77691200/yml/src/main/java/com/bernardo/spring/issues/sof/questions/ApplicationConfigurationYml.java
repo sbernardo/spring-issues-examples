4e417265da6f77fbb4781cc33248f67e5b106e69
Bernardo Salvatore <salvatore.bernardo@finconsgroup.com>
2023-12-24 12:53:28 +0100

    Updated https://stackoverflow.com/questions/77691200. Using `@Value` for yml example too instead of `ConfigurationProperties`

--- sof-questions-77691200/yml/src/main/java/com/bernardo/spring/issues/sof/questions/ApplicationConfigurationYml.java
+++ sof-questions-77691200/yml/src/main/java/com/bernardo/spring/issues/sof/questions/ApplicationConfigurationYml.java
@@ -2,27 +2,29 @@ package com.bernardo.spring.issues.sof.questions;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
+import java.util.Arrays;
+
 @Slf4j
-@EnableConfigurationProperties(ConfigurationPropertiesYml.class)
 @SpringBootApplication
 @RequiredArgsConstructor
 public class ApplicationConfigurationYml {
+
+    @Value("${gateway.publicEndpoints}")
+    private String[] endpoints;
 
     public static void main(String[] args) {
         SpringApplication.run(ApplicationConfigurationYml.class, args);
     }
 
-    private final ConfigurationPropertiesYml servers;
-
     @Bean
     ApplicationRunner runAll() {
-        return args -> log.info("Configuration {}", servers);
+        return args -> log.info("Configuration {}", Arrays.toString(endpoints));
     }
 
 }
